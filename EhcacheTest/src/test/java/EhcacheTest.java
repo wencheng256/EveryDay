e32baef62a28ef89b280bbe3a71f463684debc25
wencheng <wencheng256@gmail.com>
2016-08-15 22:32:30 +0800

    ehcache test start

--- EhcacheTest/src/test/java/EhcacheTest.java
+++ EhcacheTest/src/test/java/EhcacheTest.java
@@ -1,5 +1,9 @@
+import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.junit.Test;
+
+import java.io.File;
 
 /**
  * Created by wencheng on 16/8/14.
@@ -8,7 +12,10 @@ public class EhcacheTest {
 
     @Test
     public void test1(){
-        CacheManager.create();
+        CacheManager cm = CacheManager.getInstance();
+        cm.addCache("cache1");
+        Cache c1 = cm.getCache("cache1");
+        c1.put(new Element("111", "22222"));
     }
 
 }
